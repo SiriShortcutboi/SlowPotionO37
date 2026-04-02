6d6244ea8fed9582e169728d74a6592efa4ea228
holden.anderson <holden.anderson@students.snow.edu>
2026-04-02 17:11:03 -0600

    initial commit

---
+++ SlowPotionO37/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+					
+public class Program
+{
+	public static async Task Main()
+	{
+		await BrewPotion(); //tell it i want to wait for this to finish
+        
+	}
+
+    static async Task BrewPotion()
+        {
+            Console.WriteLine("Starting potion brew...");
+            await Task.Delay(3000);
+            Console.WriteLine("Potion finished!");
+        }
+}
