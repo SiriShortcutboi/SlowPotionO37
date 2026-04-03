01e41758f1108734ee242298c8772000cbef2f5a
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-04-02 22:21:21 -0600

    funny joke to help me remember

--- SlowPotionO37/Program.cs
+++ SlowPotionO37/Program.cs
@@ -5,7 +5,8 @@ public class Program
 	public static async Task Main()
 	{
 		await BrewPotion(); //tell it i want to wait for this to finish
-        
+        //because it's a main youre still just starting it up and running
+        //other methods through it. 
 	}
 
     static async Task BrewPotion()
@@ -15,3 +16,6 @@ public class Program
             Console.WriteLine("Potion finished!");
         }
 }
+
+//we got void main, task main, no main, 
+        //Ghost main up in this c sharp joint
